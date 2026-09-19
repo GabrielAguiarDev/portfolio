7f0d6e96f1693e7fdbe7b66abee95c514f0b65a0
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2026-09-19 12:28:42 -0300

    Remove unused development component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 const CRITICAL_VENDOR =
   /[\\/]node_modules[\\/](react|react-dom|scheduler|react-router|react-router-dom|react-i18next|i18next|i18next-browser-languagedetector|@remix-run)[\\/]/;
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -24,4 +23,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}));
+});
